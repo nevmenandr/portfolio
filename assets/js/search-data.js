ce90e2345ccfdc7af80b8781dc7eba40fc700862
nevmenandr <nevmenandr@gmail.com>
2025-09-08 17:33:33 +0000

    Deploying to gh-pages from @ nevmenandr/portfolio@da93a5e0672c0dbed2f3f392daabba69b64c6ca4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,21 +58,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/portfolio/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/portfolio/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/portfolio/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
