00fd7b3dc1d4261c9699dbf6770cd00fe3ffd053
nevmenandr <nevmenandr@gmail.com>
2025-09-08 15:01:13 +0000

    Deploying to gh-pages from @ nevmenandr/portfolio@1edd90a1c102bbac9774a4ef0978af6900836c9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Meaningfull projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio/projects/";
