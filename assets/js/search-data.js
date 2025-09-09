3c47fdea80f3f09c251f86e67305495f3438423d
nevmenandr <nevmenandr@gmail.com>
2025-09-09 22:18:07 +0000

    Deploying to gh-pages from @ nevmenandr/portfolio@fc5b3dcd0223ddb215c4aaec0790fcb3f36b72da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "учебные материалы читаемых курсов",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio/teaching/";
